71ed6a45c5a0078b4d22b520bc9328f9b6302e7c
Jasper S <jshumakerpruitt@gmail.com>
2016-09-12 22:05:58 +0700

    pass auth token when calling api

--- app/controllers/user_likes_controller.rb
+++ app/controllers/user_likes_controller.rb
@@ -4,7 +4,10 @@ class UserLikesController < ApplicationController
     id = params['like'] && params['like']['id']
     if current_user.user_likes.create(liked_user_id: id)
       render json: {"status": 201}, status: 201
+    else
+      render json: {'error': 'something went wrong'}, status: 404
     end
+
   end
 
   def index
