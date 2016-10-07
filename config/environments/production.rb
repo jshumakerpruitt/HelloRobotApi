1b93d7318618c49bbf5d932cd489cd6c90a7bfec
Jasper <jshumakerpruitt@gmail.com>
2016-10-07 13:36:50 +0700

    config websocket for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_cable.allowed_request_origins = ['https://jshumakerpruitt.github.io']
-
+  config.web_socket_server_url = "wss://okrobot.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://okrobot.herokuapp.com']
 end
